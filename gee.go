c29ace7607a8a185b30d2d0b8e310d3f4eef4f92
yujian.ou <yujian.ou@upai.com>
2021-01-07 16:52:48 +0800

    add middleware

--- gee.go
+++ gee.go
@@ -1,41 +1,119 @@
 package gee
 
 import (
+	"html/template"
+	"log"
 	"net/http"
+	"path"
+	"strings"
 )
 
 type HandlerFunc func(*Context)
 
 type Engine struct {
-	router *router
+	*RouterGroup
+	router        *router
+	groups        []*RouterGroup
+	htmlTemplates *template.Template
+	funcMap       template.FuncMap
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRouter(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRouter(method, pattern, handler)
+func Default() *Engine {
+	engine := New()
+	engine.Use(Logger(), Recovery())
+	return engine
 }
 
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRouter(http.MethodGet, pattern, handler)
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRouter(http.MethodPost, pattern, handler)
+func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
+	group.middlewares = append(group.middlewares, middlewares...)
 }
 
-func (engine *Engine) PUT(pattern string, handler HandlerFunc) {
-	engine.addRouter(http.MethodPut, pattern, handler)
+func (group *RouterGroup) addRouter(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Router %4s - %s\n", method, pattern)
+	group.engine.router.addRouter(method, pattern, handler)
 }
 
-func (engine *Engine) DELETE(pattern string, handler HandlerFunc) {
-	engine.addRouter(http.MethodDelete, pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRouter(http.MethodGet, pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRouter(http.MethodPost, pattern, handler)
+}
+
+func (group *RouterGroup) PUT(pattern string, handler HandlerFunc) {
+	group.addRouter(http.MethodPut, pattern, handler)
+}
+
+func (group *RouterGroup) DELETE(pattern string, handler HandlerFunc) {
+	group.addRouter(http.MethodDelete, pattern, handler)
+}
+
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		fileServer.ServeHTTP(c.Write, c.Req)
+	}
+}
+
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	group.GET(urlPattern, handler)
+}
+
+func (engine *Engine) SetFuncMap(funcMap template.FuncMap) {
+	engine.funcMap = funcMap
+}
+
+func (engine *Engine) LoadHTMLGlob(pattern string) {
+	engine.htmlTemplates = template.Must(template.New("").Funcs(engine.funcMap).ParseGlob(pattern))
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range engine.groups {
+		if strings.HasPrefix(r.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
+
 	c := newContext(w, r)
+	c.handlers = middlewares
+	c.engine = engine
 	engine.router.handle(c)
 }
 
--- gee_test.go
+++ gee_test.go
@@ -1,35 +1,61 @@
 package gee
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
+func logger(c *Context) {
+	start := time.Now()
+
+	c.Next()
+
+	log.Printf("[%d] %s in %v", c.StatusCode, c.Req.RequestURI, time.Since(start))
+}
+
 func TestGee(t *testing.T) {
-	e := New()
-	e.GET("/user/:id", func(c *Context) {
-		id := c.Param("id")
-		c.HTML(http.StatusOK, fmt.Sprintf("<h1> Hello, %s </h1>", id))
-	})
+	engine := New()
+	engine.Use(logger)
 
-	e.POST("/user", func(c *Context) {
-		user := c.PostForm("user")
-		c.JSON(http.StatusOK, H{
-			"user": user,
-			"form": c.Req.PostForm,
+	v1 := engine.Group("/api/v1")
+	{
+		v1.GET("/user/:id", func(c *Context) {
+			id := c.Param("id")
+			c.String(http.StatusOK, "Hello %s\n", id)
 		})
+		v1.POST("/user", func(c *Context) {
+			c.JSON(http.StatusOK, H{
+				"username": c.PostForm("user"),
+				"password": c.PostForm("pass"),
+			})
+		})
+		v1.PUT("/user/:id/update", func(c *Context) {
+			id := c.Param("id")
+			c.String(http.StatusOK, "%s update ok", id)
+		})
+		v1.DELETE("/user/:id/delete", func(c *Context) {
+			id := c.Param("id")
+			c.String(http.StatusOK, "%s delete ok", id)
+		})
+	}
+	v2 := engine.Group("/api/v2")
+	v2.Use(func(c *Context) {
+		start := time.Now()
+		c.Fail(http.StatusInternalServerError, "internal server error")
+		log.Printf("[%d] %s in %v\n", c.StatusCode, c.Req.RequestURI, time.Since(start))
 	})
+	{
+		v2.GET("/hello", func(c *Context) {
+			name := c.Query("name")
+			c.String(http.StatusOK, "hello %s", name)
+		})
+	}
 
-	e.PUT("/user/:id/update", func(c *Context) {
-		id := c.Param("id")
-		c.String(http.StatusOK, "%s update ok", id)
-	})
-
-	e.DELETE("/user/:id/delete", func(c *Context) {
-		id := c.Param("id")
-		c.Data(http.StatusOK, []byte(id))
-	})
-
-	e.Run("localhost:3434")
+	if err := engine.Run("localhost:3434"); err != nil {
+		assert.NoError(t, err)
+	}
 }
